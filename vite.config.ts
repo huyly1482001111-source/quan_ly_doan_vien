6b7b7aca6425e7f55602f484eba1f831ec6014c9
Ly Van Huy <huyly1482001111@gmail.com>
2026-02-25 00:24:52 +0800

    fix base config for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dang_vien/'   // ⚠️ PHẢI TRÙNG TÊN REPO
+  base: '/'   // hoặc xóa luôn dòng base
 })
